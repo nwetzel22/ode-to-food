52510d9666a6a08dba801014af10669f90131b55
Nick Wetzel <nwetzel41@gmail.com>
2018-06-10 19:24:21 -0400

    Initial commit

---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Models;
+using OdeToFood.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRestaurantData _restaurantData;
+
+        public HomeController(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _restaurantData.GetAll();
+
+            return View(model);
+        }
+    }
+}
---
+++ OdeToFood/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,40 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>
+            {
+                new Restaurant
+                {
+                    Id = 1,
+                    Name = "McDonald's"
+                },
+                new Restaurant
+                {
+                    Id = 2,
+                    Name = "BGR"
+                },
+                new Restaurant
+                {
+                    Id = 3,
+                    Name = "Panera"
+                }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurants.OrderBy(r => r.Name);
+        }
+
+        List<Restaurant> _restaurants;
+    }
+}
