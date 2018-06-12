68c6496411521f7f17347326b12c5c5edaa00107
Nick Wetzel <nwetzel41@gmail.com>
2018-06-11 20:36:08 -0400

    - Add HomeIndexViewModel - Configure for VS Code

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,25 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using OdeToFood.Models;
 using OdeToFood.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using OdeToFood.ViewModels;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData,
+                              IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         public IActionResult Index()
         {
-            var model = _restaurantData.GetAll();
+            var model = new HomeIndexViewModel
+            {
+                Restaurants = _restaurantData.GetAll(),
+                CurrentMessage = _greeter.GetMessageOfTheDay()
+            };
 
             return View(model);
         }
---
+++ OdeToFood/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using OdeToFood.Models;
+
+namespace OdeToFood.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public HomeIndexViewModel()
+        {
+        }
+
+        public IEnumerable<Restaurant> Restaurants
+        {
+            get;
+            set;
+        }
+
+        public string CurrentMessage
+        {
+            get;
+            set;
+        }
+    }
+}
