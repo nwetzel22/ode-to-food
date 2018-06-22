f9baa5110e6dd089637423f042b923897f43e196
nicholaswetzel <nwetzel41@gmail.com>
2018-06-22 16:15:54 -0400

    Add validation and formatting data annotations to restaurant model

--- OdeToFood/ViewModels/RestaurantEditModel.cs
+++ OdeToFood/ViewModels/RestaurantEditModel.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using OdeToFood.Models;
 
 namespace OdeToFood.ViewModels
 {
     public class RestaurantEditModel
     {
+        [Required]
+        [MaxLength(80)]
         public string Name { get; set; }
+
         public CuisineType Cuisine { get; set; }
     }
 }
