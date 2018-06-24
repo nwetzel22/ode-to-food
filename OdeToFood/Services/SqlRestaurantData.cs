aa031621419000ba6eb6e3c56a5653aed450fe64
nicholaswetzel <nwetzel41@gmail.com>
2018-06-24 17:30:29 -0400

    Configure EF with SQL Server Docker

---
+++ OdeToFood/Services/SqlRestaurantData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Data;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private OdeToFoodDbContext _odeToFoodDbContext;
+
+        public SqlRestaurantData(OdeToFoodDbContext odeToFoodDbContext)
+        {
+            _odeToFoodDbContext = odeToFoodDbContext;
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            _odeToFoodDbContext.Add(newRestaurant);
+            _odeToFoodDbContext.SaveChanges();
+            return newRestaurant;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _odeToFoodDbContext.Restaurants.Find(id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _odeToFoodDbContext.Restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
